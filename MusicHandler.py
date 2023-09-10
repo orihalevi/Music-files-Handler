830880cf156d83535a0707a2797540ecba8c578c
orihalevi <60349296+orihalevi@users.noreply.github.com>
2023-09-10 03:28:39 +0300

    Add files via upload

--- MusicHandler.py
+++ MusicHandler.py
@@ -1,8 +1,10 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QMenuBar, QAction, QToolBar, QVBoxLayout, QWidget, QLabel,
-                             QScrollArea, QLineEdit, QHBoxLayout, QCheckBox, QStatusBar, QMessageBox)
+                             QScrollArea, QLineEdit, QHBoxLayout, QCheckBox, QStatusBar, QMessageBox, QFileDialog, QDialog, QGroupBox, QGridLayout, QShortcut)
+from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QKeySequence, QFont
+from PyQt5.QtMultimedia import QSound
 import os
 import tinytag
 import re
@@ -23,6 +25,7 @@ class MainWindow(QMainWindow):
         # Setting text in status bar
         self.status_bar.showMessage("סטטוס: מוכן")
 
+        self.center_label = None
         self.input_fields_layout = None
         self.layout = None
         self.drag_area = None
@@ -50,6 +53,7 @@ class MainWindow(QMainWindow):
         self.create_bars()
         self.create_check_boxes()
         self.creating_a_frame_with_a_scroll_bar()
+        self.Add_a_label_to_the_frame()
 
         self.show()
 
@@ -57,16 +61,33 @@ class MainWindow(QMainWindow):
         # create a menu bar
         menu_bar = QMenuBar(self)
         file_menu = menu_bar.addMenu("קובץ")
+
+        open_folder_action = QAction("בחר מתיקייה", self)
+        open_folder_action.triggered.connect(self.open_file)
+        file_menu.addAction(open_folder_action)
+
         exit_action = QAction("יציאה", self)
         exit_action.triggered.connect(self.close)
         file_menu.addAction(exit_action)
+
+        tools_menu = menu_bar.addMenu("כלים")
+        Manage_json_files_action = QAction("נהל קבצי json", self)
+        Manage_json_files_action.triggered.connect(self.manage_json_files)
+        tools_menu.addAction(Manage_json_files_action)
+
         self.setMenuBar(menu_bar)
 
         # create toolbar
         tool_bar = QToolBar("סרגל כלים", self)
         self.addToolBar(tool_bar)
         # create the save button
-        save_action = QAction(QIcon(r'dependence\images\save.png'), 'שמור', self)
+        save_action = QAction(QIcon(r'dependence\images\save.png'), 'שמור (Ctrl+S)', self)
+        # Create a shortcut for Ctrl+S and Ctrl+ד
+        save_shortcut = QShortcut(QKeySequence('Ctrl+S'), self)
+        save_shortcut.activated.connect(self.save_action_triggered)
+        save_shortcut_heb = QShortcut(QKeySequence('Ctrl+ד'), self)
+        save_shortcut_heb.activated.connect(self.save_action_triggered)
+
         save_action.triggered.connect(self.save_action_triggered)
         tool_bar.addAction(save_action)
         # create the clean all button
@@ -75,6 +96,12 @@ class MainWindow(QMainWindow):
         tool_bar.addAction(clear_action)
         # create the back button
         back_action = QAction(QIcon(r'dependence\images\back.png'), "חזור", self)
+        # Create a shortcut for Ctrl+S and Ctrl+ד
+        back_shortcut = QShortcut(QKeySequence('Ctrl+Z'), self)
+        back_shortcut.activated.connect(self.restore_names_triggered)
+        back_shortcut_heb = QShortcut(QKeySequence('Ctrl+ז'), self)
+        back_shortcut_heb.activated.connect(self.restore_names_triggered)
+
         back_action.triggered.connect(self.restore_names_triggered)
         tool_bar.addAction(back_action)
 
@@ -105,8 +132,41 @@ class MainWindow(QMainWindow):
         self.input_fields_layout = QVBoxLayout()
         scroll_layout.addLayout(self.input_fields_layout)
 
+    def Add_a_label_to_the_frame(self):
+        # יצירת והוספת ה- QLabel ל- input_fields_layout
+        self.center_label = QLabel("גרור שירים לכאן\n\n\n\n\n", self)
+        self.center_label.setAlignment(QtCore.Qt.AlignCenter)  # מיון הטקסט למרכז
+        # הגדרת גופן גדול יותר לטקסט
+        font = QFont()
+        font.setPointSize(20)  # גודל הגופן בנקודות
+        self.center_label.setFont(font)
+        self.input_fields_layout.addWidget(self.center_label)
+
 # So far the initial interface has been designed
 # From here on, mainly the functions of the software operations
+
+    def manage_json_files(self):
+        json_manager_window = JSONFileManager()
+        json_manager_window.exec_()
+
+    def open_file(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly  # לא לאפשר עריכת הקובץ
+        file_paths, _ = QFileDialog.getOpenFileNames(self, "פתח קובץ", "",
+                                                     "קבצי מוזיקה (*.mp3 *.wav *.flac *.ogg *.m4a *.aac *.wma *.aiff "
+                                                     "*.opus);;כל הקבצים (*.*)",
+                                                     options=options)
+        for file_path in file_paths:
+            file_ext = os.path.splitext(file_path)[1].lower()
+            if file_ext.lower() not in ['.mp3', '.wav', '.flac', '.ogg', '.m4a', '.aac', '.wma', '.aiff', '.opus']:
+                self.status_bar.showMessage("הבאת סוג קובץ שאינו נתמך")
+                continue
+            if file_path in self.audio_files_paths:  # בדיקה שהקובץ אינו כבר ברשימה
+                self.status_bar.showMessage("הבאת שיר שכבר נמצא ברשימה")
+                continue
+            self.audio_files_paths.append(file_path)
+            self.apply_text_corrections(file_path)
+            self.add_song_info_fields()
 
     def dragLeaveEvent(self, event):
         # self.drag_area.setStyleSheet("background-color: white; border: 2px dashed gray;")
@@ -127,10 +187,11 @@ class MainWindow(QMainWindow):
                 self.status_bar.showMessage("גררת סוג קובץ שאינו נתמך")
                 continue
             if file_path in self.audio_files_paths:  # Checking that the path is not already in the list
-                self.status_bar.showMessage("גררת פריט שהינו כבר ברשימה")
+                self.status_bar.showMessage("גררת שיר שהינו כבר ברשימה")
                 continue
             self.audio_files_paths.append(file_path)
             self.apply_text_corrections(file_path)
+            self.center_label.clear()
             self.add_song_info_fields()
 
     def apply_text_corrections(self, file_path):
@@ -236,7 +297,7 @@ class MainWindow(QMainWindow):
     def add_song_info_fields(self):
         # Creates a content line of the song details within the frame
         input_row_layout = QHBoxLayout()
-        self.input_fields_layout.addLayout(input_row_layout)
+        self.input_fields_layout.insertLayout(0, input_row_layout)
         # Adding the content to the frame by order
         self.add_input_field(input_row_layout, f"{self.year}", is_year=True)
         self.add_input_field(input_row_layout, f"{self.title}", is_title=True)
@@ -309,6 +370,7 @@ class MainWindow(QMainWindow):
 # From here on, the buttons actions
 
     def save_action_triggered(self):
+        QSound.play("C:/Windows/Media/chimes.wav")
         self.previous_names.clear()
         duplicate_files = {}  # מילון לאחסון קבצים כפולים (שם קובץ: רשימת נתיבים)
         for i, path in enumerate(self.audio_files_paths):
@@ -395,6 +457,7 @@ class MainWindow(QMainWindow):
             msg_box.exec_()
 
     def clear_action_triggered(self):
+        QSound.play("C:/Windows/Media/Windows Recycle.wav")
         # ניקוי רשימות הנתיבים
         self.audio_files_paths.clear()
         self.new_file_paths.clear()
@@ -410,8 +473,10 @@ class MainWindow(QMainWindow):
             item = self.input_fields_layout.takeAt(0)
             if item.widget():
                 item.widget().deleteLater()
+        self.Add_a_label_to_the_frame()
 
     def restore_names_triggered(self):
+        QSound.play("C:/Windows/Media/chord.wav")
         if self.previous_names:
             for key, values in self.previous_names.items():
                 original_name = values[0]  # קריאה לאיבר הראשון שברשימת הערכים
@@ -427,7 +492,200 @@ class MainWindow(QMainWindow):
             print("Sdasdadadasdasdasdasdasdasdasd")
 
 
+class JSONFileManager(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("ניהול קבצי JSON")
+        self.setGeometry(150, 150, 1400, 800)
+
+        # Create a scroll area
+        scroll_area = QScrollArea(self)
+        scroll_area.setWidgetResizable(True)
+        self.scroll_content = QWidget(scroll_area)
+        scroll_area.setWidget(self.scroll_content)
+
+        # Create a QHBoxLayout for adding the QGroupBoxes side by side
+        layout = QHBoxLayout(self.scroll_content)
+
+        # JSON files
+        json_files = ["Title.json", "AlbumArtist.json", "ContributingArtists.json", "TrackNumber.json", "Year.json"]
+
+        # Create QGroupBoxes for each JSON file
+        self.group_boxes = {}  # Store references to the group boxes
+        for json_file_name in json_files:
+            group_box = QGroupBox(json_file_name)
+            self.group_layout = QVBoxLayout()  # QVBoxLayout for each group box
+
+            # Load JSON data and create QLineEdit widgets
+            json_data = self.load_json_data(json_file_name)
+            has_empty_row = False  # Flag to track if an empty row already exists
+
+            for key, value in json_data.items():
+                row_layout = QHBoxLayout()  # QHBoxLayout for each row
+
+                # Create QLineEdit for the key
+                key_edit = QLineEdit(key)
+                key_edit.textChanged.connect(self.update_empty_row_status)
+                row_layout.addWidget(key_edit)
+
+                # Create QLineEdit for the value
+                value_edit = QLineEdit(value)
+                value_edit.textChanged.connect(self.update_empty_row_status)
+                row_layout.addWidget(value_edit)
+
+                # Add the row to the group box layout
+                self.group_layout.addLayout(row_layout)
+
+                # Check if this row is empty (both key and value are empty strings)
+                if key == "" and value == "":
+                    has_empty_row = True
+
+            # Add an empty row with two empty QLineEdit widgets if one doesn't exist
+            if not has_empty_row:
+                empty_row_layout = QHBoxLayout()
+                empty_key_edit = QLineEdit()
+                empty_key_edit.textChanged.connect(self.update_empty_row_status)
+                empty_value_edit = QLineEdit()
+                empty_value_edit.textChanged.connect(self.update_empty_row_status)
+                empty_row_layout.addWidget(empty_key_edit)
+                empty_row_layout.addWidget(empty_value_edit)
+                self.group_layout.addLayout(empty_row_layout)
+
+
+            # Set the layout of the group box
+            group_box.setLayout(self.group_layout)
+
+            # Add the group box to the main layout
+            layout.addWidget(group_box)
+
+            # Store a reference to the group box
+            self.group_boxes[json_file_name] = group_box
+
+        # Set the layout of scroll_content to the QHBoxLayout
+        self.scroll_content.setLayout(layout)
+
+        # Set up the scroll area in the main layout
+        main_layout = QVBoxLayout(self)
+
+        # Create a toolbar and add a "Save" action
+        toolbar = QToolBar("סרגל כלים", self)
+        main_layout.addWidget(toolbar)
+
+        save_action = QAction(QIcon(r'dependence\images\save.png'), 'שמור (Ctrl+S)', self)
+        # Create a shortcut for Ctrl+S and Ctrl+ד
+        save_shortcut = QShortcut(QKeySequence('Ctrl+S'), self)
+        save_shortcut.activated.connect(self.save_data_to_json)
+        save_shortcut_heb = QShortcut(QKeySequence('Ctrl+ד'), self)
+        save_shortcut_heb.activated.connect(self.save_data_to_json)
+
+        save_action.triggered.connect(self.save_data_to_json)
+        toolbar.addAction(save_action)
+
+        # Add the scroll area containing the QGroupBoxes
+        main_layout.addWidget(scroll_area)
+
+        self.setLayout(main_layout)
+
+    def load_json_data(self, json_file_name):
+        json_path = os.path.join('dependence', 'DicFiles', json_file_name)
+        try:
+            with open(json_path, 'r', encoding='utf-8') as json_file:
+                return json.load(json_file)
+        except FileNotFoundError:
+            return {}  # Return an empty dictionary if the file is not found
+
+    def save_data_to_json(self):
+        QSound.play("C:/Windows/Media/chimes.wav")
+        # Iterate through the stored group boxes and save the data to JSON files
+        for json_file_name, group_box in self.group_boxes.items():
+            json_data = {}
+            for index in range(group_box.layout().count()):
+                row_layout = group_box.layout().itemAt(index)
+                key_edit = row_layout.itemAt(0).widget()
+                value_edit = row_layout.itemAt(1).widget()
+                key = key_edit.text()
+                value = value_edit.text()
+                json_data[key] = value
+
+            json_path = os.path.join('dependence', 'DicFiles', json_file_name)
+            with open(json_path, 'w', encoding='utf-8') as json_file:
+                json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+
+    def add_empty_row(self, json_file_name):
+        # Get the current group box
+        group_box = self.group_boxes[json_file_name]
+
+        # Check if there are any empty rows in the group box
+        empty_row_found = False
+        for index in range(group_box.layout().count()):
+            row_layout = group_box.layout().itemAt(index)
+            key_edit = row_layout.itemAt(0).widget()
+            value_edit = row_layout.itemAt(1).widget()
+            if key_edit.text() == "" and value_edit.text() == "":
+                empty_row_found = True
+                break
+
+        # If no empty row was found, add a new empty row with two QLineEdit widgets
+        if not empty_row_found:
+            empty_row_layout = QHBoxLayout()
+            empty_key_edit = QLineEdit()
+            empty_key_edit.textChanged.connect(self.update_empty_row_status)
+            empty_value_edit = QLineEdit()
+            empty_value_edit.textChanged.connect(self.update_empty_row_status)
+            empty_row_layout.addWidget(empty_key_edit)
+            empty_row_layout.addWidget(empty_value_edit)
+
+            # Add the empty row layout to the group box layout
+            group_box.layout().addLayout(empty_row_layout)
+
+    def update_empty_row(self, json_file_name, remove=True):
+        # Get the current group box
+        group_box = self.group_boxes[json_file_name]
+
+        # Create a list to store the indices of empty rows
+        empty_row_indices = []
+
+        # Find and store the indices of empty rows
+        for index in range(group_box.layout().count()):
+            row_layout = group_box.layout().itemAt(index)
+            key_edit = row_layout.itemAt(0).widget()
+            value_edit = row_layout.itemAt(1).widget()
+            if key_edit.text() == "" and value_edit.text() == "":
+                empty_row_indices.append(index)
+
+        # Check if there are at least two empty rows
+        if len(empty_row_indices) >= 2:
+            # Remove the first empty row found
+            index_to_remove = empty_row_indices[1]
+            row_layout = group_box.layout().itemAt(index_to_remove)
+            for i in range(row_layout.count()):
+                item = row_layout.itemAt(i)
+                widget = item.widget()
+                if widget:
+                    widget.deleteLater()  # Remove the widget from the layout
+            row_layout.deleteLater()  # Remove the layout itself
+
+
+    def update_empty_row_status(self):
+        # Check if there are any empty rows in each group box and update the status label
+        for json_file_name, group_box in self.group_boxes.items():
+            empty_row_found = False
+            for index in range(group_box.layout().count()):
+                row_layout = group_box.layout().itemAt(index)
+                key_edit = row_layout.itemAt(0).widget()
+                value_edit = row_layout.itemAt(1).widget()
+                if key_edit.text() == "" and value_edit.text() == "":
+                    empty_row_found = True
+                    break
+
+            # Update the status label for the current group box
+            if empty_row_found:
+                self.update_empty_row(json_file_name)
+            else:
+                self.add_empty_row(json_file_name)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    json_manager = JSONFileManager()  # יצירת החלון לניהול קבצי JSON
     sys.exit(app.exec_())
